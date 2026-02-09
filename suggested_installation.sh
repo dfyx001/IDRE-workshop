80411639c54896d58dd600fb2c81440971aa2eea
DONGXIAO CHEN <cdx@g19.hoffman2.idre.ucla.edu>
2026-02-08 22:52:47 -0800

    Sun Feb  8 22:52:47 PST 2026

---
+++ suggested_installation.sh
@@ -0,0 +1,9 @@
+conda create -p $(pwd)/idre-env python=3.10 -y
+conda activate $(pwd)/idre-env
+conda install -c conda-forge ase pyscf -y
+module load gcc/11.3.0 intel/2020.4 intel/mpi
+pip install gpaw
+module load cuda/11.8
+pip install torch --index-url https://download.pytorch.org/whl/cu118
+pip install mace-torch
+
