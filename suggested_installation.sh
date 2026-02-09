479cdc4370f34cb28cb29bdce2b3803f3eeeb250
DONGXIAO CHEN <cdx@login3.hoffman2.idre.ucla.edu>
2026-02-08 23:33:25 -0800

    Sun Feb  8 23:33:25 PST 2026

--- suggested_installation.sh
+++ suggested_installation.sh
@@ -1,7 +1,7 @@
 conda create -p $(pwd)/idre-env python=3.10 -y
 conda init bash
 source ~/.bashrc
-conda activate $(pwd)/idre-env
+conda activate -p $(pwd)/idre-env
 conda install -c conda-forge ase pyscf -y
 module load gcc/11.3.0 intel/2020.4 intel/mpi
 pip install gpaw
